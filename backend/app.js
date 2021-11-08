48e71fad0c58931918e5a339a0b035d2396d1a0d
MoHoss2000 <61802989+MoHoss2000@users.noreply.github.com>
2021-11-08 21:54:08 +0200

    test

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,7 @@ const port = process.env.port || '8000'
 app.use(express.json());
 app.use(cors());
 
+
 mongoose.connect(process.env.DBLINK, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(result =>console.log("MongoDB is now connected") )
 .catch(err => console.log(err));

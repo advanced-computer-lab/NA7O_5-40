a49044efe25e0cca3e8639b821709baa9ea012df
alibarbary <alibarbary1324@outlook.com>
2021-11-28 22:20:03 +0200

    Delete and confirm

--- backend/routes/userController.js
+++ backend/routes/userController.js
@@ -1,103 +1,76 @@
-const express = require('express');
-const Flight = require('../models/flightSchema');
-const User = require('../models/userSchema');
-const Reservation = require('../models/reservationSchema');
-const endOfDayfrom = require('date-fns/endOfDay')
-const startOfDay = require('date-fns/startOfDay');
+const express = require("express");
+const Flight = require("../models/flightSchema");
+const User = require("../models/userSchema");
+const Reservation = require("../models/reservationSchema");
 
 const router = express.Router();
 
-router.post('/update', async (req, res) => {
-    console.log(req.body);
-    User.findByIdAndUpdate(
-        req.body._id,
-        req.body,
-        function (err, newUser) {
-            if (err) {
-                //duplicate key error
-                if (err.code === 11000) {
-                    res.status(400).send('User does not exist');
-                    return;
-                }
+router.post("/update", async (req, res) => {
+  console.log(req.body);
+  User.findByIdAndUpdate(req.body._id, req.body, function (err, newUser) {
+    if (err) {
+      //duplicate key error
+      if (err.code === 11000) {
+        res.status(400).send("User does not exist");
+        return;
+      }
 
-                return res.status(400).send(err.codeName)
-            }
-
-            res.status(200).send('User updated');
-        });
-})
-
-router.get('/:id', async (req, res) => {
-    console.log(req.params.id);
-    User.findById(req.params.id, function (err, user) {
-        if (!err) {
-            res.status(200).send(user)
-        }
-        else {
-            console.log(err)
-            return res.status(400).send(err);
-        }
-    });
-});
-
-router.get('/reservations/:id', async (req, res) => {
-    console.log(req.params.id);
-    Reservation.find({ userId: req.params.id }, function (err, reservation) {
-        if (!err) {
-            res.status(200).send(reservation)
-        }
-        else {
-            console.log(err)
-            return res.status(400).send(err);
-        }
-    });
-});
-
-router.post('/Reservation/create', async (req, res) => {
-    console.log(req.body)
-    const newReservation = new Reservation(
-        req.body
-    );
-
-    try {
-        await newReservation.save();
-        res.status(200).send('Reservation added successfully');
-    } catch (e) {
-        if (e.code === 11000) {
-            res.status(400).send('Reservation no already exists');
-            return;
-        }
-
-        res.status(400).send(e.codeName);
+      return res.status(400).send(err.codeName);
     }
+
+    res.status(200).send("User updated");
+  });
 });
 
-router.post('/flights/search', async (req, res) => {
-    console.log(req.body)
-
-    var adults = req.body.adults;
-    var children = req.body.children;
-    var depAirport = req.body.depAirport;
-    var arrivalAirport = req.body.arrivalAirport;
-    var depDate = req.body.depDate;
-    var cabinClass = req.body.cabinClass;
-
-    var noOfSeats = adults + children;
-
-    var candidateFlights = Flight.find(
-        {
-            departureAirport: depAirport,
-            arrivalAirport: arrivalAirport,
-            departureDate: {
-                $gte: startOfDay(depDate),
-                $lte: endOfDay(depDate),
-            }
-        });
-
-
+router.get("/:id", async (req, res) => {
+  console.log(req.params.id);
+  User.findById(req.params.id, function (err, user) {
+    if (!err) {
+      res.status(200).send(user);
+    } else {
+      console.log(err);
+      return res.status(400).send(err);
+    }
+  });
+});
+router.delete("/reservations/:id", async (req, res) => {
+  console.log("fss");
+  console.log(req.params.id);
+  try {
+    await Reservation.findByIdAndRemove(req.params.id).exec();
+    res.send("succesfully deleted!");
+  } catch (err) {
+    console.log(err);
+  }
 });
 
+router.get("/reservations/:id", async (req, res) => {
+  console.log(req.params.id);
+  Reservation.find({ userId: req.params.id }, function (err, reservation) {
+    if (!err) {
+      res.status(200).send(reservation);
+    } else {
+      console.log(err);
+      return res.status(400).send(err);
+    }
+  });
+});
 
+router.post("/Reservation/create", async (req, res) => {
+  console.log(req.body);
+  const newReservation = new Reservation(req.body);
 
+  try {
+    await newReservation.save();
+    res.status(200).send("Reservation added successfully");
+  } catch (e) {
+    if (e.code === 11000) {
+      res.status(400).send("Reservation no already exists");
+      return;
+    }
+
+    res.status(400).send(e.codeName);
+  }
+});
 
 module.exports = router;
--- frontend/src/pages/DisplayReservations/DisplayReservations.jsx
+++ frontend/src/pages/DisplayReservations/DisplayReservations.jsx
@@ -1,62 +1,114 @@
-
-import React, { useEffect, useState } from 'react';
-import { DataGrid } from '@mui/x-data-grid';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import Axios from 'axios'
+import React, { useEffect, useState } from "react";
+import { DataGrid } from "@mui/x-data-grid";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import Axios from "axios";
+import IconButton from "@mui/material/IconButton";
+import Button from "@mui/material/Button";
+import DeleteIcon from "@mui/icons-material/Delete";
+import axios from "axios";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
 export default function DisplayReservations() {
-    const [reservations, setReservations] = useState([]);
+  const [reservations, setReservations] = useState([]);
 
-    const getReservationsFromBE = () => {
-        const id = '61a28ba03853507d6e8e2f46';
-        Axios.get(`http://localhost:8000/user/reservations/${id}`).then((response) => {
-            console.log(response.data);
-            setReservations(response.data);
-        }).catch((e) => {
-            console.log(e)
-        })
-    }
-    useEffect(() => {
-        getReservationsFromBE();
-    }, []);
+  const getReservationsFromBE = () => {
+    const id = "61a28ba03853507d6e8e2f46";
+    Axios.get(`http://localhost:8000/user/reservations/${id}`)
+      .then((response) => {
+        console.log(response.data);
+        setReservations(response.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+  const deletereservation = (id) => {
+    axios.delete(`http://localhost:8000/user/reservations/${id}`).then(() => {
+      console.log(id);
+      window.location.reload(false);
+    });
+  };
+  useEffect(() => {
+    getReservationsFromBE();
+  }, []);
+  const [open, setOpen] = React.useState(false);
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
 
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-    
-        return (
-            <TableContainer component={Paper}>
-                <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell allign="left"> Departure Flight</TableCell>
-                            <TableCell align="left">Return Flight</TableCell>
-                            <TableCell align="left">Booking no.</TableCell>
-                            <TableCell align="left">Cabin no.</TableCell>
-                            <TableCell align="left">Seat no.</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {reservations.map((reservation) => (
-                            <TableRow
-                               
-                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                            >
-                                
-                                <TableCell align="left">{reservation.departureFlightId}</TableCell>
-                                <TableCell align="left">{reservation.returnFlightId}</TableCell>
-                                <TableCell align="left">{reservation.bookingNumber}</TableCell>
-                                <TableCell align="left">{reservation.chosenCabin}</TableCell>
-                                <TableCell align="left">{reservation.chosenSeatNumber}</TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-        );
-    
-
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell allign="left"> Departure Flight</TableCell>
+            <TableCell align="left">Return Flight</TableCell>
+            <TableCell align="left">Booking no.</TableCell>
+            <TableCell align="left">Cabin no.</TableCell>
+            <TableCell align="left">Seat no.</TableCell>
+            <TableCell align="left">Actions</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {reservations.map((reservation) => (
+            <TableRow
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell align="left">
+                {reservation.departureFlightId}
+              </TableCell>
+              <TableCell align="left">{reservation.returnFlightId}</TableCell>
+              <TableCell align="left">{reservation.bookingNumber}</TableCell>
+              <TableCell align="left">{reservation.chosenCabin}</TableCell>
+              <TableCell align="left">{reservation.chosenSeatNumber}</TableCell>
+              <TableCell align="left">
+                <Button
+                  variant="outlined"
+                  startIcon={<DeleteIcon />}
+                  onClick={handleClickOpen}
+                >
+                  cancel reservation
+                </Button>
+                <Dialog
+                  open={open}
+                  onClose={handleClose}
+                  aria-labelledby="alert-dialog-title"
+                  aria-describedby="alert-dialog-description"
+                >
+                  <DialogTitle id="alert-dialog-title">
+                    {"Are you sure you want to delete?"}
+                  </DialogTitle>
+                  <DialogActions>
+                    <Button onClick={handleClose}>Decline</Button>
+                    <Button
+                      onClick={() => {
+                        deletereservation(reservation._id);
+                        handleClose();
+                      }}
+                      autoFocus
+                    >
+                      Confirm
+                    </Button>
+                  </DialogActions>
+                </Dialog>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 }

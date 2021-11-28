65600daa445c3f6ae790872030a2426a31ddcf53
youssefashourr <youssefashour96@yahoo.com>
2021-11-28 20:00:29 +0200

    v2

--- backend/routes/userController.js
+++ backend/routes/userController.js
@@ -1,75 +1,76 @@
-const express = require('express');
-const Flight = require('../models/flightSchema');
-const User = require('../models/userSchema');
-const Reservation = require('../models/reservationSchema');
+const express = require("express");
+const Flight = require("../models/flightSchema");
+const User = require("../models/userSchema");
+const Reservation = require("../models/reservationSchema");
 
 const router = express.Router();
 
-router.post('/update', async (req, res) => {
-    console.log(req.body);
-    User.findByIdAndUpdate(
-        req.body._id,
-        req.body,
-        function (err, newUser) {
-            if (err) {
-                //duplicate key error
-                if (err.code === 11000) {
-                    res.status(400).send('User does not exist');
-                    return;
-                }
+router.post("/update", async (req, res) => {
+  console.log(req.body);
+  User.findByIdAndUpdate(req.body._id, req.body, function (err, newUser) {
+    if (err) {
+      //duplicate key error
+      if (err.code === 11000) {
+        res.status(400).send("User does not exist");
+        return;
+      }
 
-                return res.status(400).send(err.codeName)
-            }
+      return res.status(400).send(err.codeName);
+    }
 
-            res.status(200).send('User updated');
-        });
-})
+    res.status(200).send("User updated");
+  });
+});
 
-router.get('/:id',async(req,res)=>{
-    console.log(req.params.id);
-    User.findById(req.params.id, function (err, user) {
-        if (!err) {
-            res.status(200).send(user)
-        }
-        else {
-            console.log(err)
-            return res.status(400).send(err);
-        }
-    });
-    });
-    
-    router.get('/reservations/:id',async(req,res)=>{
-        console.log(req.params.id);
-        Reservation.find({userId:req.params.id}, function (err, reservation) {
-            if (!err) {
-                res.status(200).send(reservation)
-            }
-            else {
-                console.log(err)
-                return res.status(400).send(err);
-            }
-        });
-        });
-        
-        router.post('/Reservation/create', async (req, res) => {
-            console.log(req.body)
-            const newReservation = new Reservation(
-                req.body
-            );
-        
-            try {
-                await newReservation.save();
-                res.status(200).send('Reservation added successfully');
-            } catch (e) {
-                if (e.code === 11000) {
-                    res.status(400).send('Reservation no already exists');
-                    return;
-                }
-        
-                res.status(400).send(e.codeName);
-            }
-        });
+router.get("/:id", async (req, res) => {
+  console.log(req.params.id);
+  User.findById(req.params.id, function (err, user) {
+    if (!err) {
+      res.status(200).send(user);
+    } else {
+      console.log(err);
+      return res.status(400).send(err);
+    }
+  });
+});
+router.delete("/reservations/:id", async (req, res) => {
+  console.log("fss");
+  console.log(req.params.id);
+  try {
+    await Reservation.findByIdAndRemove(req.params.id).exec();
+    res.send("succesfully deleted!");
+  } catch (err) {
+    console.log(err);
+  }
+});
 
-   
+router.get("/reservations/:id", async (req, res) => {
+  console.log(req.params.id);
+  Reservation.find({ userId: req.params.id }, function (err, reservation) {
+    if (!err) {
+      res.status(200).send(reservation);
+    } else {
+      console.log(err);
+      return res.status(400).send(err);
+    }
+  });
+});
+
+router.post("/Reservation/create", async (req, res) => {
+  console.log(req.body);
+  const newReservation = new Reservation(req.body);
+
+  try {
+    await newReservation.save();
+    res.status(200).send("Reservation added successfully");
+  } catch (e) {
+    if (e.code === 11000) {
+      res.status(400).send("Reservation no already exists");
+      return;
+    }
+
+    res.status(400).send(e.codeName);
+  }
+});
 
 module.exports = router;
